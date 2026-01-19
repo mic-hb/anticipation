99a913365117e83e42309e4476a3b9a6be339b60
tempoxylophone <15273090+tempoxylophone@users.noreply.github.com>
2026-01-18 23:00:32 -0500

    add lakh test case

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@
 import tempfile
 import importlib
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Any
 
 import pytest
 
@@ -20,6 +20,12 @@ def c_major_midi_path() -> Path:
     return TEST_DATA_PATH / "cmajor.mid"
 
 @pytest.fixture
+def lmd_0_example_midi_path() -> Path:
+    # from The Lakh MIDI Dataset v0.1 (https://colinraffel.com/projects/lmd/)
+    # from split 0, first file in name order
+    return TEST_DATA_PATH / "0a0a2b0e4d3b7bf4c5383ba025c4683e.mid"
+
+@pytest.fixture(scope="function")
 def patch_config_and_reload(monkeypatch: pytest.MonkeyPatch) -> TestConfigPatcher:
     """
     Pytest fixture for changing the values of:
@@ -28,17 +34,27 @@ def patch_config_and_reload(monkeypatch: pytest.MonkeyPatch) -> TestConfigPatche
     Any code that imports those values must be reloaded, otherwise the
     changes do not take effect.
     """
+    import anticipation.tokenize as anticipation_tokenize_cl
+    import anticipation.vocab as anticipation_vocab
+    from anticipation import config as anticipation_config
     def _apply(**kwargs):
-        from anticipation import config
         for k, v in kwargs.items():
-            monkeypatch.setattr(config, k, v)
-        import anticipation.tokenize as anticipation_tokenize_cl
-        import anticipation.vocab as anticipation_vocab
+            monkeypatch.setattr(anticipation_config, k, v)
         importlib.reload(anticipation_vocab)
         importlib.reload(anticipation_tokenize_cl)
-    return _apply
+    yield _apply
+    # undo any changes done to the config by reloading it (and things
+    # that depend on it) during teardown
+    importlib.reload(anticipation_config)
+    importlib.reload(anticipation_vocab)
+    importlib.reload(anticipation_tokenize_cl)
 
-def get_tokens_from_midi_file(midi_file_path: Path, augment_factor: int = 10) -> dict[str, str]:
+
+def get_tokens_from_midi_file(
+    midi_file_path: Path,
+    augment_factor: int = 10,
+    return_original_compound: bool = False
+) -> dict[str, Any]:
     assert midi_file_path.exists()
     assert midi_file_path.is_file()
 
@@ -67,7 +83,7 @@ def get_tokens_from_midi_file(midi_file_path: Path, augment_factor: int = 10) ->
             augment_factor=augment_factor
         )
         midi_tokens = Path(output_fname).read_text()
-        return {
+        parse_info = {
             "midi_tokens": midi_tokens,
             "seqcount": seqcount,
             "rest_count": rest_count,
@@ -76,4 +92,9 @@ def get_tokens_from_midi_file(midi_file_path: Path, augment_factor: int = 10) ->
             "num_too_many_instruments": num_too_many_instruments,
             "num_inexpressible": num_inexpressible,
             "all_truncations": all_truncations,
+            "compound": None,
         }
+        if return_original_compound:
+            parse_info["compound"] = midi_preprocess_token_list
+
+        return parse_info
--- tests/test_tokenize_routines.py
+++ tests/test_tokenize_routines.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
-from conftest import patch_config_and_reload
-from tests.conftest import get_tokens_from_midi_file, TestConfigPatcher
 from anticipation import config
+import anticipation.vocab as v
+
+from tests.conftest import get_tokens_from_midi_file, TestConfigPatcher, patch_config_and_reload
+
 
 def test_patch_config_at_test_time(patch_config_and_reload: TestConfigPatcher) -> None:
     # this is an edit that makes no sense, we wouldn't set this to 0
@@ -13,9 +15,89 @@ def test_config_patch_rolled_back() -> None:
     # test that changes from test patching is isolated
     assert config.COMPOUND_SIZE == 5
 
-def test_ar_tokenization(c_major_midi_path: Path, patch_config_and_reload: TestConfigPatcher) -> None:
-    # we have 966 total tokens, model context is 1024
-    patch_config_and_reload(MIN_TRACK_EVENTS=0, MIN_TRACK_TIME_IN_SECONDS=0, M=256)
-    parse_info = get_tokens_from_midi_file(c_major_midi_path, augment_factor=10)
+def test_tokenization_small_sequence_ar(c_major_midi_path: Path, patch_config_and_reload: TestConfigPatcher) -> None:
+    m = 10
+    event_size = 3
+    augment_factor = 1
+    patch_config_and_reload(MIN_TRACK_EVENTS=0, MIN_TRACK_TIME_IN_SECONDS=0, M=m)
+    parse_info = get_tokens_from_midi_file(
+        c_major_midi_path,
+        augment_factor=augment_factor,
+        return_original_compound=True
+    )
+    compound = parse_info["compound"]
+    print(compound)
+    sequences = parse_info["midi_tokens"].strip().split("\n")
+
+    assert len(compound) == 145
+    assert parse_info["seqcount"] == 3
+    assert len(sequences) == parse_info["seqcount"]
+
+    # check each sequence
+    seq_1 = [int(x) for x in sequences[0].split(" ")]
+    seq_2 = [int(x) for x in sequences[1].split(" ")]
+    seq_3 = [int(x) for x in sequences[2].split(" ")]
+    assert len(seq_1) == (1 + (m * event_size))
+    assert len(seq_2) == (1 + (m * event_size))
+    assert len(seq_3) == (1 + (m * event_size))
+
+    instr = 0
+    # 50 is quarter note in 4/4 120 BPM
+    assert seq_1 == [
+        v.AUTOREGRESS,
+        v.SEPARATOR, v.SEPARATOR, v.SEPARATOR,
+        # (time, duration, instrument x note_value)
+        v.TIME_OFFSET + 0, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 36), # C1
+        v.TIME_OFFSET + 50, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 38), # D1
+        v.TIME_OFFSET + 100, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 40), # E1
+        v.TIME_OFFSET + 150, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 41), # F1
+        v.TIME_OFFSET + 200, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 43), # G1
+        v.TIME_OFFSET + 250, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 45), # A1
+        v.TIME_OFFSET + 300, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 47), # B1
+        v.TIME_OFFSET + 350, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 48), # C2
+        v.TIME_OFFSET + 450, v.DUR_OFFSET + 50, v.NOTE_OFFSET + (config.MAX_PITCH * instr + 50), # D2
+    ]
+    assert seq_2 == [
+        v.AUTOREGRESS,
+        # (time, duration, instrument x note_value)
+        0, 10050, 11052,
+        50, 10050, 11053,
+        100, 10050, 11055,
+        150, 10050, 11057,
+        200, 10050, 11059,
+        250, 10050, 11060,
+        350, 10050, 11062,
+        400, 10050, 11064,
+        450, 10050, 11065,
+        500, 10050, 11067
+    ]
+    assert seq_3 == [
+        v.AUTOREGRESS,
+        # (time, duration, instrument x note_value)
+        0, 10050, 11069,
+        50, 10050, 11071,
+        100, 10050, 11072,
+        200, 10050, 11074,
+        250, 10050, 11076,
+        300, 10050, 11077,
+        350, 10050, 11079,
+        400, 10050, 11081,
+        450, 10050, 11083,
+        500, 10050, 11084
+    ]
+
+
+def test_tokenization_lakh_example(lmd_0_example_midi_path: Path) -> None:
+    assert config.M == 341
+    parse_info = get_tokens_from_midi_file(lmd_0_example_midi_path, augment_factor=10)
     midi_tokens = parse_info["midi_tokens"]
-    assert len(midi_tokens) == 4276
+    seqcount = parse_info["seqcount"]
+    assert parse_info["rest_count"] == 185
+    assert parse_info["num_too_short"] == 0
+    assert parse_info["num_too_long"] == 0
+    assert parse_info["num_too_many_instruments"] == 0
+    assert parse_info["num_inexpressible"] == 0
+    assert parse_info["all_truncations"] == 0
+    assert seqcount == 84
+    assert len(midi_tokens.strip().split("\n")) == seqcount
+    assert len(midi_tokens) == 494_036
