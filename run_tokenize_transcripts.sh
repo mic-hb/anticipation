9b2877b8c8c673209be81f15acf8a134f6b04d50
mf867 <mf867@cornell.edu>
2026-02-18 11:40:02 -0500

    add script for tokenization transcripts

---
+++ run_tokenize_transcripts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p thickstun
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=50GB
+#SBATCH -t 100:00:00
+#SBATCH -J train_ar_local_midi_v2
+#SBATCH -e output/slurm_logs/%j/stderr.err
+#SBATCH -o output/slurm_logs/%j/stdout.out
+set -e
+
+# --- set up conda and activate it ---
+# assuming conda binary lives here
+CONDA_ACTIVATE_PATH="/share/apps/software/anaconda3/etc/profile.d/conda.sh"
+if source "$CONDA_ACTIVATE_PATH" 2>/dev/null; then
+  cd /home/mf867/anticipation_isolated/anticipation
+  conda activate ./env
+  echo "activated environment."
+else
+  echo "conda startup script not found."
+fi
+
+
+# TODO: need to make lmd vs. transcripts toggle-able by argparse
+PYTHONPATH=. python train/v2/dataset_tokenize.py
